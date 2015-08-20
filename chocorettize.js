af29fd4773741f7e9892c4a83c056ed19bc83824
Javier Matusevich <javiermatusevich@Javiers-MacBook.local>
2015-08-20 10:36:11 -0300

    built

--- chocorettize.js
+++ chocorettize.js
@@ -6,7 +6,7 @@ function walk(node) {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
 	var child, next;
-	
+
 	switch (node.nodeType) {
 		case 1:  // Element
 		case 9:  // Document
@@ -26,11 +26,10 @@ function walk(node) {
 
 function handleText(textNode)  {
 	var v = textNode.nodeValue;
-	
-    v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-    v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+
+    v = v.replace(/\bMicaela Chinelli\b/g, "Miku Chocoretto");
+	v = v.replace(/\bmicaela chinelli\b/g, "Miku Chocoretto");
+    v = v.replace(/\b Chinelli\b/g, "Chocoretto");
+	v = v.replace(/\bmicaela chinelli\b/g, "Miku Chocoretto");
 	textNode.nodeValue = v;
 }
